c1b27fec9a282fbeb886b2d7178de8b84021a372
Andreas Eckhart <151761363+andreas-eckhart@users.noreply.github.com>
2026-05-11 16:38:43 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='CCARemote',
-    version='1.0.0',
-    description='ESP32 & Pico 2 Dashboard: Flexible Steuerung per BLE & WLAN',
-    author='Andreas Eckhart',
-    packages=['CCARemote'], # Hier muss der Name des Unterordners stehen
-)
